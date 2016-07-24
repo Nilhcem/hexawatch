7c3f0817440e8d82b01e2191fdf5f58460049231
Gautier Nilhcem <nilhcem@gmail.com>
2016-07-24 10:07:40 +0200

    Start creating configuration activities

---
+++ app/src/main/java/com/nilhcem/hexawatch/ui/config/CompanionConfigActivity.java
@@ -0,0 +1,7 @@
+package com.nilhcem.hexawatch.ui.config;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CompanionConfigActivity extends AppCompatActivity {
+
+}
---
+++ wear/src/main/java/com/nilhcem/hexawatch/ui/config/WatchConfigActivity.java
@@ -0,0 +1,6 @@
+package com.nilhcem.hexawatch.ui.config;
+
+import android.app.Activity;
+
+public class WatchConfigActivity extends Activity {
+}
